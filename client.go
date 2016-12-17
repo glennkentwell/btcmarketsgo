2d0c0827bf53b3d6d1caec002933e4fe37c9c8f7
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-12-18 01:12:23 +1000

    so much progress

--- client.go
+++ client.go
@@ -3,9 +3,10 @@ package btcmarketsgo
 //BTCMarketsClient is primary struct for interacting with the API
 type BTCMarketsClient struct {
 	signiture string
+	api       string
 }
 
 //NewClient gets a new BTCMarketsClient
-func NewClient(signiture string) *BTCMarketsClient {
-	return &BTCMarketsClient{signiture: signiture}
+func NewClient(api, signiture string) *BTCMarketsClient {
+	return &BTCMarketsClient{signiture: signiture, api: api}
 }
