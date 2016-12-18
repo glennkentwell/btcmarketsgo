5a77bc90f152bbef2ddbbe52f5ceb45495a497b4
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-12-18 22:04:38 +1000

    work u fagggot

--- test/main.go
+++ test/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/RyanCarrier/btcmarketsgo"
 )
@@ -9,17 +14,50 @@ import (
 var public string
 var private string
 
-func init() {
-	fmt.Println("Public API key:")
-	fmt.Scanln(&public)
-	fmt.Println("Private API key:")
-	fmt.Scanln(&private)
+const apiPath = "api.secret"
 
+func init() {
+	getKeys()
 }
 
 func main() {
 	client, err := btcmarketsgo.NewClient(public, private)
 	if err == nil {
-		fmt.Println(client.Tick())
+		got, err := client.Tick()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%+v\n", got)
 	}
+}
+
+func getKeys() {
+	if _, err := os.Stat(apiPath); err == nil {
+		getKeysFile()
+	} else {
+		getKeysStdin()
+	}
+}
+
+func getKeysFile() {
+	all, err := ioutil.ReadFile(apiPath)
+	if err != nil {
+		log.Error("error reading file", err)
+		getKeysStdin()
+		return
+	}
+	keys := strings.Fields(strings.TrimSpace(string(all)))
+	if len(keys) != 2 {
+		log.Error("fix your api.secret file with public, then private. Nothing else.")
+		getKeysStdin()
+	} else {
+		public, private = keys[0], keys[1]
+	}
+}
+
+func getKeysStdin() {
+	fmt.Println("Public API key:")
+	fmt.Scanln(&public)
+	fmt.Println("Private API key:")
+	fmt.Scanln(&private)
 }
