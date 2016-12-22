8ae3dc024ecd991e316f4417df82e8af02ccb493
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-12-22 14:08:02 +1100

    clean up ticker

--- ticker.go
+++ ticker.go
@@ -3,14 +3,12 @@ package btcmarketsgo
 import "time"
 
 //Ticker sets up a ticker to run the function provided every duration specified, quits when true is passed through quit chan
-func (c BTCMarketsClient) Ticker(fn func(TickResponse, error), d time.Duration, quit chan bool) {
+func (c BTCMarketsClient) Ticker(fn func(TickResponse, error), d time.Duration, quitChan chan bool) {
 	go func() {
-		for {
+		for quit := false; !quit; {
 			select {
-			case val := <-quit:
-				if val {
-					return
-				}
+			case quit = <-quitChan:
+				break
 			default:
 				fn(c.Tick())
 				time.Sleep(d)
