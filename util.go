e1a0a74324dc8fcb37e828a8ca7bf12b5564a1bf
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-12-25 01:22:29 +1100

    cleanup debugg

--- util.go
+++ util.go
@@ -7,7 +7,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -63,7 +62,6 @@ func (c BTCMarketsClient) signAnd(URI string, i interface{}, do string) ([]byte,
 	} else {
 		body = []byte("")
 	}
-	fmt.Println(string(body))
 	client := http.Client{}
 	now, signature := c.sign(URI, string(body))
 	URL := c.Domain + URI
